a86d4c601653d7dd9ce3d5fcd5719223461673b9
Toomas <39192575+eix-1eq0@users.noreply.github.com>
2019-06-20 15:13:43 +0300

    Create STAR-CCM_SLURM.sh

---
+++ STAR-CCM_SLURM.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+SBATCH --job-name=STAR-CCM+_example
+SBATCH --ntasks=12
+SBATCH --exclusive
+
+#Ensures that module loads
+source /etc/profile.d/modules.sh
+
+#STAR-CCM+ license
+export CDLMD_LICENSE_FILE=1999@flex.cd-adapco.com
+
+#STAR-CCM+ license key (not in the case of power on demand)
+#LM_PROJCET=
+
+#Scaratch
+mkdir /state/partition1/starccmtmp
+export STAR_MASTER_TMPDIR=/state/partition1/starccmtmp
+
+#STAR-CCM+ module
+module load STAR-CCM+
+
+#STAR-CCM+ run computation
+starccm+ -power -podkey YOUR_KEY_HERE -batch run -np 12 $@
+
+#Remove STAR-CCM+ temporary files
+sleep 10
+rm -rf /state/partition1/starccmtmp
